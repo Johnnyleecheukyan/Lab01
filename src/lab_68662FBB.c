c6d49f951ec6e498224a7640d0dcf32d38f774fb
Johnnyleecheukyan <s2659088@student.ctihe.edu.hk>
2026-09-17 11:45:52 +0800

    print big a

--- src/lab_68662FBB.c
+++ src/lab_68662FBB.c
@@ -2,7 +2,16 @@
 
 void printscreen()
 {
-    // Please write your code here
+   printf("#####\n");
+   printf("#   #\n");
+   printf("#   #\n");
+   printf("#####\n");
+   printf("#   #\n");
+   printf("#   #\n");
+   printf("#   #\n");
+   printf("#   #\n");
+   
+
 }
 
 // DO NOT change the code below
